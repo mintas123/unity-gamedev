465343b08833860bde1861223fe295af9d85c1cc
mintas123 <52136988+mintas123@users.noreply.github.com>
2021-08-11 23:47:09 +0200

    particles and sounds

--- Project2 - Project Boost/Assets/Scripts/CollisionHandler.cs
+++ Project2 - Project Boost/Assets/Scripts/CollisionHandler.cs
@@ -3,26 +3,97 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [SerializeField] float delay = 1f;
+    [SerializeField] AudioClip crashSound;
+    [SerializeField] AudioClip successSound;
+
+    [SerializeField] ParticleSystem crashParticles;
+    [SerializeField] ParticleSystem successParticles;
+
+    AudioSource audioSource;
+
+    bool isTransitioning = false;
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
     private void OnCollisionEnter(Collision other)
     {
+        if (isTransitioning) return;
+
 
         switch (other.gameObject.tag)
         {
             case "Friendly":
-                Debug.Log("bff");
                 break;
             case "Finish":
-                Debug.Log("You are done!");
+                StartSuccessSequence();
                 break;
             default:
-                Debug.Log("beng");
-                ReloadLevel();
+                StartCrashSequence();
                 break;
-
-
         }
     }
 
+    private void StartSuccessSequence()
+    {
+        isTransitioning = true;
+
+
+        var movementScript = GetComponent<Movement>();
+        movementScript.enabled = false;
+
+        //reset rotation
+        var rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        rb.MoveRotation(Quaternion.AngleAxis(5f, Vector3.up));
+
+        //turn off sounds && start success
+        audioSource.Stop();
+        audioSource.PlayOneShot(successSound);
+        successParticles.Play();
+
+
+        Invoke("NextLevel", delay);
+
+
+    }
+
+    private void NextLevel()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        Invoke("ReloadLevel", delay);
+        SceneManager.LoadScene(nextSceneIndex);
+
+    }
+    private void StartCrashSequence()
+    {
+        isTransitioning = true;
+
+
+        //disable movement
+        var movementScript = GetComponent<Movement>();
+        movementScript.enabled = false;
+        Invoke("ReloadLevel", delay);
+
+        //turn off sounds && start crash
+        audioSource.Stop();
+        audioSource.PlayOneShot(crashSound);
+        crashParticles.Play();
+
+
+
+    }
     private void ReloadLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
--- Project2 - Project Boost/Assets/Scripts/Movement.cs
+++ Project2 - Project Boost/Assets/Scripts/Movement.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-    new Rigidbody rigidbody;
-    AudioSource audioSource;
+
     [SerializeField] float thrust = 100f;
     [SerializeField] float agility = 100f;
+    [SerializeField] ParticleSystem boostParticles;
+    [SerializeField] ParticleSystem leftThrustParticles;
+    [SerializeField] ParticleSystem rightThrustParticles;
+
+    [SerializeField] AudioClip mainEngine;
+
+    new Rigidbody rigidbody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,11 +39,14 @@ public class Movement : MonoBehaviour
             rigidbody.AddRelativeForce(Vector3.up * thrust * Time.deltaTime);
             if (!audioSource.isPlaying)
             {
-                audioSource.Play();
+                audioSource.PlayOneShot(mainEngine);
+                boostParticles.Play();
+
             }
         }
         else
         {
+            boostParticles.Stop();
             audioSource.Stop();
         }
 
@@ -46,17 +56,29 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
         {
+            leftThrustParticles.Play();
+            rightThrustParticles.Play();
             Debug.Log("stall");
         }
         else
         {
             if (Input.GetKey(KeyCode.LeftArrow))
             {
+                rightThrustParticles.Stop();
+                leftThrustParticles.Play();
                 ApplyRotation(agility);
+
             }
             else if (Input.GetKey(KeyCode.RightArrow))
             {
+                leftThrustParticles.Stop();
+                rightThrustParticles.Play();
                 ApplyRotation(-agility);
+            }
+            else
+            {
+                rightThrustParticles.Stop();
+                leftThrustParticles.Stop();
             }
         }
 
