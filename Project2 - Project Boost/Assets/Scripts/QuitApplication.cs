973883cc3b47bf9e07475a58616fdd77057ce48f
mintas123 <52136988+mintas123@users.noreply.github.com>
2021-09-03 23:25:07 +0200

    oscillating obstacles, finish

---
+++ Project2 - Project Boost/Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
+
+    }
+}
